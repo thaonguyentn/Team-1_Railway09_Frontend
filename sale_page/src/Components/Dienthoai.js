66e238ea5d31d8318eb5fa0d808c12c8469213c0
xmen1xb <xmen1xb@gmailcom>
2021-09-17 21:44:56 +0700

    fixed

--- sale_page/src/Components/Dienthoai.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Dienthoai extends Component {
-  render() {
-    return <div>Điện Thoại</div>;
-  }
-}
-
-export default Dienthoai;
--- sale_page/src/Components/Laptop.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Laptop extends Component {
-  render() {
-    return <div>Laptop</div>;
-  }
-}
-
-export default Laptop;
