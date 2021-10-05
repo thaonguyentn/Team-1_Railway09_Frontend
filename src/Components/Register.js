893c212ad903a314e900c420a8a83558d7ee40bd
xmen1xb <xmen1xb@gmailcom>
2021-10-05 08:24:26 +0700

    fix

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -23,7 +23,7 @@ function Register(props) {
       seterrorusername("* Không được để trống");
     }
     console.log(body);
-    Axios.post("http://localhost:8080/api/v3/register", body)
+    Axios.post("https://thegioicamtayapi.herokuapp.com/api/v3/register", body)
       .then((response) => {
         setisopen(false);
         alert(
@@ -33,7 +33,7 @@ function Register(props) {
         );
       })
       .catch((errors) => {
-        console.log(errors.response.data.status);
+        console.log(errors);
       });
   };
   return (
