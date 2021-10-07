856aaddc8806251f5f1385ed788c6b6c79db7794
xmen1xb <xmen1xb@gmailcom>
2021-10-08 02:21:47 +0700

    add author

--- src/Requestdata/CallAPI.js
+++ src/Requestdata/CallAPI.js
@@ -1,16 +1,12 @@
 import Axios from "axios";
 const baseURL = "https://thegioicamtayapi.herokuapp.com";
 export const getbrand = () => {
-  const get = Axios.get(baseURL + "/api/v1/productbrandcontrollers", {
-    auth: {
-      username: "adminaccount",
-      password: "123456",
-    },
-  });
+  let token = localStorage.getItem("token");
+  const get = Axios.get(baseURL + "/api/v1/productbrandcontrollers");
   return get;
 };
 export const getcart = (id) => {
-  const token = localStorage.getItem("token");
+  let token = localStorage.getItem("token");
   const get = Axios.get(baseURL + "/api/v5/cart/" + id, {
     headers: {
       Authorization: "Bearer " + token,
@@ -19,68 +15,90 @@ export const getcart = (id) => {
   return get;
 };
 export const getcartdetail = (id) => {
+  let token = localStorage.getItem("token");
   const get = Axios.get(baseURL + "/api/v5/cart/" + id + "/cartDetails", {
-    auth: {
-      username: "adminaccount",
-      password: "123456",
+    headers: {
+      Authorization: "Bearer " + token,
     },
   });
   return get;
 };
 export const getmemory = () => {
-  const get = Axios.get(baseURL + "/api/v1/productmemorycontrollers", {
-    auth: {
-      username: "adminaccount",
-      password: "123456",
-    },
-  });
+  const get = Axios.get(baseURL + "/api/v1/productmemorycontrollers");
   return get;
 };
 export const getorder = (id) => {
-  const get = Axios.get(baseURL + "/api/v5/orders/" + id);
-  return get;
-};
-export const getallorder = (page) => {
-  if (page === undefined || page === "") {
-    page = 1;
-  }
-  const get = Axios.get(baseURL + "/api/v5/orders/all?page=" + page);
-  return get;
-};
-export const getorderdetail = (id) => {
-  const get = Axios.get(baseURL + "/api/v5/orders/" + id + "/orderDetails");
-  return get;
-};
-export const getaccount = (id) => {
-  const get = Axios.get(baseURL + "/api/v1/accounts/" + id);
-  return get;
-};
-export const getram = () => {
-  const get = Axios.get(baseURL + "/api/v1/productramcontrollers", {
-    auth: {
-      username: "adminaccount",
-      password: "123456",
+  let token = localStorage.getItem("token");
+  const get = Axios.get(baseURL + "/api/v5/orders/" + id, {
+    headers: {
+      Authorization: "Bearer " + token,
     },
   });
   return get;
 };
+export const getallorder = (page) => {
+  let token = localStorage.getItem("token");
+  if (page === undefined || page === "") {
+    page = 1;
+  }
+  const get = Axios.get(baseURL + "/api/v5/orders/all?page=" + page, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+  return get;
+};
+export const getorderdetail = (id) => {
+  let token = localStorage.getItem("token");
+  const get = Axios.get(baseURL + "/api/v5/orders/" + id + "/orderDetails", {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+  return get;
+};
+export const getram = () => {
+  const get = Axios.get(baseURL + "/api/v1/productramcontrollers");
+  return get;
+};
 export const addquantity = (id) => {
-  const post = Axios.put(baseURL + "/api/v4/cartdetail/" + id);
+  let token = localStorage.getItem("token");
+  const post = Axios.put(baseURL + "/api/v4/cartdetail/" + id, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
   return post;
 };
 export const minusquantity = (id) => {
-  const post = Axios.put(baseURL + "/api/v4/cartdetail/?id=" + id);
+  let token = localStorage.getItem("token");
+  const post = Axios.put(baseURL + "/api/v4/cartdetail/?id=" + id, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
   return post;
 };
 export const changestatuscart = (id) => {
-  const get = Axios.post(baseURL + "/api/v4/cartdetail/" + id);
+  let token = localStorage.getItem("token");
+  const get = Axios.post(baseURL + "/api/v4/cartdetail/" + id, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
   return get;
 };
 export const getcartdetailbyid = (id) => {
-  const get = Axios.get(baseURL + "/api/v4/cartdetail/" + id);
+  let token = localStorage.getItem("token");
+  const get = Axios.get(baseURL + "/api/v4/cartdetail/" + id, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
   return get;
 };
 export const getlistaccount = (page) => {
+  let token = localStorage.getItem("token");
   const token = localStorage.getItem("token");
   if (page === undefined || page === "") {
     page = 1;
@@ -108,13 +126,7 @@ export const getlistproduct = (page, ram, brand, memory, search) => {
       rams +
       brands +
       memorys +
-      searchs,
-    {
-      auth: {
-        username: "adminaccount",
-        password: "123456",
-      },
-    }
+      searchs
   );
   return get;
 };
@@ -129,11 +141,12 @@ export const getlistproductsort = (order, page) => {
   return get;
 };
 export const createproduct = (body) => {
-  const create = Axios.post(baseURL + "/api/v2/products", body).then(
-    (response) => {
-      console.log(response);
-    }
-  );
+  let token = localStorage.getItem("token");
+  const create = Axios.post(baseURL + "/api/v2/products", body, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
   return create;
 };
 export const activeaccount = (token) => {
@@ -141,7 +154,7 @@ export const activeaccount = (token) => {
   return active;
 };
 export const deleteproduct = (id) => {
-  const token = localStorage.getItem("token");
+  let token = localStorage.getItem("token");
   const deleteproduct = Axios.delete(baseURL + "/api/v2/products/" + id, {
     headers: {
       Authorization: "Bearer " + token,
@@ -150,20 +163,36 @@ export const deleteproduct = (id) => {
   return deleteproduct;
 };
 export const cancelorder = (id, body) => {
-  const cancel = Axios.put(baseURL + "/api/v5/orders/?orderID=" + id, body);
+  let token = localStorage.getItem("token");
+  const cancel = Axios.put(baseURL + "/api/v5/orders/?orderID=" + id, body, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
   return cancel;
 };
 export const updateorder = (id) => {
-  const update = Axios.put(baseURL + "/api/v5/orders/" + id);
+  let token = localStorage.getItem("token");
+  const update = Axios.put(baseURL + "/api/v5/orders/" + id, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
   return update;
 };
 export const addcartdetail = (productid, accountid) => {
+  let token = localStorage.getItem("token");
   const add = Axios.post(
     baseURL +
       "/api/v4/cartdetail?productId=" +
       productid +
       "&accountId=" +
-      accountid
+      accountid,
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
   );
   return add;
 };
@@ -172,16 +201,23 @@ export const getlogin = (body) => {
   return login;
 };
 export const getprofile = (id, auth) => {
+  let token = localStorage.getItem("token");
   const get = Axios.get(baseURL + "/api/v1/accounts/" + id, {
     auth: auth,
   });
   return get;
 };
 export const createorder = (body) => {
+  let token = localStorage.getItem("token");
   const user_login_infor = JSON.parse(localStorage.getItem("user_login_infor"));
   const create = Axios.post(
     baseURL + "/api/v5/orders/" + user_login_infor.id,
-    body
+    body,
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
   );
   return create;
 };
